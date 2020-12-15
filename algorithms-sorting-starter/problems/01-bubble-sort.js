3ed863bfcb0f2457f8d00d89edbfe677ac22541a
whitelotus718 <70615488+whitelotus718@users.noreply.github.com>
2020-12-15 13:56:10 -0500

    bubble sort

--- algorithms-sorting-starter/problems/01-bubble-sort.js
+++ algorithms-sorting-starter/problems/01-bubble-sort.js
@@ -1,13 +1,32 @@
 // Implement Bubble Sort
+// 
+
 
 function swap(array, idx1, idx2) {
   let temp = array[idx1];
   array[idx1] = array[idx2];
   array[idx2] = temp;
+  console.log(array)
 }
 
+const arr1 = [3,4,1,5,2]
+bubbleSort(arr1)
+
 function bubbleSort(array) {
-  // your code here
+  let isSorted = false; // is undefined falsey?
+  while (!isSorted) {
+    for (let i=0; i <= array.length - 2; i++){
+      console.log(i)
+      if (array[i] > array[i + 1]) {
+        console.log("testing")
+        swap(array, i, i+1)
+        break
+      } 
+      if (i === array.length - 2) {
+        isSorted = true;
+      }
+    }
+  }
 }
 
 module.exports = {
